00dbfaf57a9a442a4eb036db4b7eb06873f50177
Dmitry Omelchenko <omd1337@gmail.com>
2022-04-15 15:41:56 +0300

    add gradient

--- src/scripts/util/extra.ts
+++ src/scripts/util/extra.ts
@@ -32,3 +32,33 @@ export class Swear {
         this._reject(data);
     };
 }
+
+export function makeGradient(
+    textElm: Phaser.GameObjects.Text,
+    options: Array<{ color: string; percent: number }>
+) {
+    const height = textElm.height;
+    const font = Number(textElm.style.fontSize.replace("px", ""));
+    const lines = Math.floor(height / font) || 1;
+
+    if (options.length < 2)
+        return console.error("at least two colors are expected");
+
+    const gradient = textElm.context.createLinearGradient(
+        0,
+        5,
+        0,
+        textElm.height
+    );
+    new Array(lines).fill("").forEach((item, index) => {
+        options.forEach((option) => {
+            gradient.addColorStop(
+                (1 / lines / 100) * option.percent + (1 / lines) * index,
+                option.color
+            );
+        });
+    });
+
+    textElm.setFill(gradient);
+    return gradient;
+}
